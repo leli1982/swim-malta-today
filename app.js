051878593259983974ba9e56ee9d691ac655b9fd
leli1982 <lelijax@yahoo.com>
2026-05-25 22:38:34 +0200

    Add installable PWA

--- app.js
+++ app.js
@@ -486,3 +486,15 @@ function startApp() {
 }
 
 startApp();
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => {
+        console.log("Service worker registered");
+      })
+      .catch(error => {
+        console.log("Service worker failed:", error);
+      });
+  });
+}
---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "swim-malta-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
